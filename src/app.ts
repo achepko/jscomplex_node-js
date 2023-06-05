c5c04d8352719ce4186761c22dd0853c8955bc1b
Anton Chepko <choptonys@gmail.com>
2023-06-05 21:28:40 +0200

    base TS + eslint + sorter of imports LAST INIT

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,79 @@
-import { request } from "express";
-import express from "express";
-import { response } from "express";
+import express, { Request, Response } from "express";
 
-import {sayHelloFunc} from "./helper";
+const users = [
+  {
+    name: "Oleh",
+    age: 20,
+    gender: "male",
+  },
+  {
+    name: "Anton",
+    age: 10,
+    gender: "male",
+  },
+  {
+    name: "Inokentiy",
+    age: 25,
+    gender: "female",
+  },
+  {
+    name: "Anastasiya",
+    age: 15,
+    gender: "female",
+  },
+  {
+    name: "Cocos",
+    age: 25,
+    gender: "other",
+  },
+];
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/users", (req: Request, res: Response) => {
+  res.status(200).json(users);
+});
+
+app.get("/users/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  res.status(200).json(users[+id]);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  users.push(req.body);
+
+  res.status(201).json({
+    message: "User created.",
+  });
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  users[+id] = req.body;
+
+  res.status(200).json({
+    message: "User updated",
+    data: users[+id],
+  });
+});
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  users.splice(+id, 1);
+
+  res.status(200).json({
+    message: "User deleted",
+  });
+});
+
+const PORT = 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server has started on PORT ${PORT} 🥸`);
+});

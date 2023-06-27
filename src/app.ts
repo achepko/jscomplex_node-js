de5745f901797ae2cd912e0f53178bddb9c56ca8
Anton Chepko <choptonys@gmail.com>
2023-06-27 22:22:15 +0200

    hw9.2 - swagger swagger.json

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import * as mongoose from "mongoose";
+import * as swaggerUi from "swagger-ui-express";
 
 import { configs } from "./configs/config";
 import { cronRunner } from "./crons";
 import { authRouter } from "./routers/auth.router";
 import { userRouter } from "./routers/user.router";
+import * as swaggerJson from "./utils/swagger.json";
 
 const app = express();
 
@@ -20,6 +22,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     status: err.status,
   });
 });
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.listen(configs.PORT, () => {
   mongoose.connect(configs.DB_URL);

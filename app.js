a8ea454fc873e57b6693e1211bd7a58ca49941bb
Anton Chepko <choptonys@gmail.com>
2023-07-13 23:55:17 +0200

    hw.12.1 - sokets

--- app.js
+++ app.js
@@ -1,57 +1,36 @@
-// hw1
+const express = require("express");
+const app = express();
+const http = require("node:http");
+const socketIO = require("socket.io");
 
-const path = require('path');
-const fs = require('fs');
+const server = http.createServer(app);
 
-fs.mkdirSync(path.join(__dirname,'folder'),(err)=>{
-    if (err) {
-        throw new Error(err.message);
-    }
-    else{
-        console.log(`folder was successfully created `)
-    }
+const io = socketIO(server, {cors: {origin: "*"}});
+
+io.on("connection", (socket) => {
+    // console.log(socket);
+
+    socket.on("message:create", (messageData) => {
+        console.log(messageData, 'Message data')
+        socket.emit("message:receive", {ok: true});
+    })
+
+    // socket.on("broadcast:all",()=>{
+    //     io.emit("alert","Повітряна тривога");
+    // })
+
+    socket.on("broadcast:all",()=>{
+        socket.broadcast.emit("alert","Повітряна тривога");
+    })
+
+    socket.on("room:joinUser",({roomId})=>{
+        socket.join(roomId);
+        io.to(roomId).emit("room:newUserAlert",socket.id);
+    })
 })
 
-const folders = ['folder1','folder2','folder3','folder4','folder5'];
 
-for (let i=0;i<folders.length;i++){
-    const folderPath= path.join(__dirname,'folder',`${folders[i]}`);
-    fs.mkdir(folderPath,(err)=>{
-        if (err) {
-            throw new Error(err.message);
-        }
-        else{
-            console.log(`${folders[i]} was successfully created `)
-        }
-    })
-}
 
-const files =['file1','file2','file3','file4','file5'];
-
-for (let i=0;i<files.length;i++){
-    const filePath = path.join(__dirname,'folder',`${files[i]}.txt`);
-    fs.writeFile(filePath,`content of ${files[i]}.txt`,(err)=>{
-        if (err){
-            throw new Error(err.message);
-        }
-        else{
-            console.log(`${files[i]} was successfully created in folder - ${folders[i]}`)
-        }
-    })
-}
-
-fs.readdir(path.join(__dirname,'folder'),{withFileTypes:true},(err,files)=>{
-    if (err){
-        throw new Error(err.message);
-    }
-    else{
-        files.forEach(file=>{
-            if (file.isFile()){
-               console.log(`FILE: ${file.name}`)
-            }
-            else{
-                console.log(`FOLDER: ${file.name}`)
-            }
-        })
-    }
+server.listen(3000, () => {
+    console.log('Listen', 3000);
 })
